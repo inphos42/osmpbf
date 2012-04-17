57bfb422ccafcb050ced70a3d7b07a5eae945f20
Oliver Groß <neokensuke@freenet.de>
2012-04-17 11:43:19 +0200

    fixed reference counting

--- refcountobject.h
+++ refcountobject.h
@@ -1,15 +1,27 @@
 #ifndef OSMPBF_REFCOUNTOBJECT_H
 #define OSMPBF_REFCOUNTOBJECT_H
 
+#include <cstdint>
+
 namespace osmpbf {
 	class RefCountObject {
 	public:
 		RefCountObject() : m_ref(0) {}
+		virtual ~RefCountObject() {}
 		
-		inline void refInc() { m_ref++; }
-		inline void refDec() { m_ref--; if (m_ref < 1) delete this; }
+		inline void refInc() {
+			m_ref++;
+		}
+		inline void refDec() {
+			m_ref--;
+			
+			if (m_ref < 1)
+				delete this;
+		}
+		
+		inline int refCounter() const { return m_ref; }
 	private:
-		int m_ref;
+		uint32_t m_ref;
 	};
 }
 
