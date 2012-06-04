1b8aadaac0cc8b4345dc70cc2309f058bade81d9
Oliver Groß <neokensuke@freenet.de>
2012-06-04 16:57:10 +0200

    finished node output implementation

--- primitiveblockoutputadaptor.cpp
+++ primitiveblockoutputadaptor.cpp
@@ -1,13 +1,21 @@
 #include "primitiveblockoutputadaptor.h"
 #include "osmformat.pb.h"
 #include "iway.h"
+#include "oway.h"
 #include "stringtable.h"
+#include "onode.h"
+#include "inode.h"
 
 namespace osmpbf {
 
 // PrimitiveBlockOutputAdaptor
 
-	PrimitiveBlockOutputAdaptor::PrimitiveBlockOutputAdaptor() : m_WaysGroup(NULL) {
+	PrimitiveBlockOutputAdaptor::PrimitiveBlockOutputAdaptor() :
+		m_PlainNodesGroup(NULL),
+		m_DenseNodesGroup(NULL),
+		m_WaysGroup(NULL),
+		m_RelationsGroup(NULL)
+	{
 		GOOGLE_PROTOBUF_VERIFY_VERSION;
 
 		m_StringTable = new StringTable();
@@ -22,11 +30,72 @@ namespace osmpbf {
 		delete m_PrimitiveBlock;
 	}
 
+	ONode PrimitiveBlockOutputAdaptor::createNode(NodeType type) {
+		switch (type) {
+		case DenseNode:
+			if (!m_DenseNodesGroup)
+				m_DenseNodesGroup = m_PrimitiveBlock->add_primitivegroup();
+
+			return ONode(new NodeOutputAdaptor(this, m_DenseNodesGroup->add_nodes()));
+		case PlainNode:
+			if (!m_PlainNodesGroup)
+				m_PlainNodesGroup = m_PrimitiveBlock->add_primitivegroup();
+
+			return ONode(new NodeOutputAdaptor(this, m_PlainNodesGroup->add_nodes()));
+		default:
+			return ONode();
+		}
+	}
+
+	ONode PrimitiveBlockOutputAdaptor::createNode(INode & templateINode) {
+		return createNode(templateINode, templateINode.internalNodeType());
+	}
+
+	ONode PrimitiveBlockOutputAdaptor::createNode(INode & templateINode, NodeType type) {
+		PrimitiveGroup * targetGroup;
+		switch (type) {
+		case DenseNode:
+			if (!m_DenseNodesGroup)
+				m_DenseNodesGroup = m_PrimitiveBlock->add_primitivegroup();
+			targetGroup = m_DenseNodesGroup;
+			break;
+		case PlainNode:
+			if (!m_PlainNodesGroup)
+				m_PlainNodesGroup = m_PrimitiveBlock->add_primitivegroup();
+			targetGroup = m_PlainNodesGroup;
+			break;
+		default:
+			return ONode();
+		}
+
+		ONode result = ONode(new NodeOutputAdaptor(this, targetGroup->add_nodes()));
+
+		//set field for new node
+		result.setId(templateINode.id());
+		result.setLati(templateINode.lati());
+		result.setLoni(templateINode.loni());
+		for (int i = 0; i < templateINode.tagsSize(); i++)
+			result.addTag(templateINode.key(i), templateINode.value(i));
+
+		return result;
+	}
+
+	int PrimitiveBlockOutputAdaptor::nodesSize (NodeType type) const {
+		switch (type) {
+		case PlainNode:
+			return m_PlainNodesGroup ? m_PlainNodesGroup->nodes_size() : 0;
+		case DenseNode:
+			return m_DenseNodesGroup ? m_DenseNodesGroup->nodes_size() : 0;
+		default:
+			return -1;
+		}
+	}
+
 	OWay PrimitiveBlockOutputAdaptor::createWay() {
 		if (!m_WaysGroup)
-			m_WaysGroup = m_PrimitiveBlock->mutable_primitivegroup()->Add();
+			m_WaysGroup = m_PrimitiveBlock->add_primitivegroup();
 
-		return OWay(new WayOutputAdaptor(this, m_WaysGroup->mutable_ways()->Add()));
+		return OWay(new WayOutputAdaptor(this, m_WaysGroup->add_ways()));
 	}
 
 	OWay PrimitiveBlockOutputAdaptor::createWay(const IWay & templateIWay) {
@@ -41,7 +110,7 @@ namespace osmpbf {
 		return result;
 	}
 
-	int PrimitiveBlockOutputAdaptor::waysSize() {
+	int PrimitiveBlockOutputAdaptor::waysSize() const {
 		return m_WaysGroup ? m_WaysGroup->ways_size() : 0;
 	}
 
@@ -83,6 +152,24 @@ namespace osmpbf {
 	}
 
 	template<typename Element>
+	void deltaEncode(Element * from, Element * to) {
+		if (from == to)
+			return;
+
+		Element * it = from;
+		Element prev = 0;
+		Element delta;
+
+		while (it != to) {
+			delta = *it - prev;
+			prev = *it;
+			*it = delta;
+
+			it++;
+		}
+	}
+
+	template<typename Element>
 	int cleanUp(Element * from, Element * to, Element clearValue) {
 		if (from == to)
 			return 0;
@@ -104,65 +191,133 @@ namespace osmpbf {
 		return target - from + 1;
 	}
 
+	template<typename PrimitiveType>
+	void cleanUpTags(PrimitiveType & primitive, uint32_t * stringIdTable) {
+		int realSize;
+
+		// clean keys
+		realSize = cleanUp<uint32_t>(primitive.mutable_keys()->mutable_data(), primitive.mutable_keys()->mutable_data() + primitive.keys_size(), 0);
+		primitive.mutable_keys()->Truncate(realSize);
+
+		// clean vals
+		realSize = cleanUp<uint32_t>(primitive.mutable_vals()->mutable_data(), primitive.mutable_vals()->mutable_data() + primitive.vals_size(), 0);
+		primitive.mutable_vals()->Truncate(realSize);
+
+		// correct string ids
+		for (int i = 0; i < primitive.keys_size(); i++) {
+			primitive.set_keys(i, stringIdTable[primitive.keys(i)]);
+			primitive.set_vals(i, stringIdTable[primitive.vals(i)]);
+		}
+	}
+
+	uint32_t * PrimitiveBlockOutputAdaptor::prepareStringTable() {
+		uint32_t * stringIdTable = new uint32_t[m_StringTable->maxId()];
+		for (uint32_t i = 0; i < m_StringTable->maxId(); i++)
+			stringIdTable[i] = 0;
+
+		// build string id table and fill string table output cache
+		int newId = 1;
+		std::map<uint32_t, StringTableEntry *>::const_iterator stringIt = m_StringTable->begin();
+		while (stringIt != m_StringTable->end()) {
+			stringIdTable[stringIt->first] = newId;
+			m_PrimitiveBlock->mutable_stringtable()->add_s(stringIt->second->value);
+			newId++;
+			++stringIt;
+		}
+
+		// we don't need the old string table anymore
+		m_StringTable->clear();
+
+		return stringIdTable;
+	}
+
+	void PrimitiveBlockOutputAdaptor::prepareNodes(PrimitiveGroup * nodesGroup, uint32_t * stringIdTable) {
+		google::protobuf::RepeatedPtrField<Node>::iterator nodeIt = nodesGroup->mutable_nodes()->begin();
+		while (nodeIt != nodesGroup->mutable_nodes()->end()) {
+			// calculate coordinates
+			nodeIt->set_lat((nodeIt->lat() - m_PrimitiveBlock->lat_offset()) / m_PrimitiveBlock->granularity());
+			nodeIt->set_lon((nodeIt->lon() - m_PrimitiveBlock->lon_offset()) / m_PrimitiveBlock->granularity());
+
+			cleanUpTags<Node>(*nodeIt, stringIdTable);
+			++nodeIt;
+		}
+	}
+
 	bool PrimitiveBlockOutputAdaptor::flush(std::string & output) {
 		if (!m_PrimitiveBlock->IsInitialized())
 			return false;
 
 		// prepare string table output cache
-		uint32_t * stringIdTable = new uint32_t[m_StringTable->maxId()];
-		{
-			for (uint32_t i = 0; i < m_StringTable->maxId(); i++)
-				stringIdTable[i] = 0;
+		uint32_t * stringIdTable = prepareStringTable();
 
-			// build string id table and fill string table output cache
-			int newId = 1;
-			std::map<int, StringTableEntry *>::const_iterator stringIt = m_StringTable->begin();
-			while (stringIt != m_StringTable->end()) {
-				stringIdTable[stringIt->first] = newId;
-				m_PrimitiveBlock->mutable_stringtable()->add_s(stringIt->second->value);
-				newId++;
-				++stringIt;
+		// prepare plain nodes
+		if (m_PlainNodesGroup)
+			prepareNodes(m_PlainNodesGroup, stringIdTable);
+
+		// prepare dense nodes
+		if (m_DenseNodesGroup) {
+			prepareNodes(m_DenseNodesGroup, stringIdTable);
+
+			int64_t prevLat = 0, prevLon = 0, prevId = 0;
+			google::protobuf::RepeatedPtrField<Node>::const_iterator nodeIt = m_DenseNodesGroup->nodes().begin();
+			while (nodeIt != m_DenseNodesGroup->nodes().end()) {
+				m_DenseNodesGroup->mutable_dense()->add_id(nodeIt->id() - prevId);
+				m_DenseNodesGroup->mutable_dense()->add_lat(nodeIt->lat() - prevLat);
+				m_DenseNodesGroup->mutable_dense()->add_lon(nodeIt->lon() - prevLon);
+
+				prevId = nodeIt->id();
+				prevLat = nodeIt->lat();
+				prevLon = nodeIt->lon();
+
+				const uint32_t * keys = nodeIt->keys().data();
+				const uint32_t * vals = nodeIt->vals().data();
+				for (int i = 0; i < nodeIt->keys_size(); ++i) {
+					m_DenseNodesGroup->mutable_dense()->add_keys_vals(keys[i]);
+					m_DenseNodesGroup->mutable_dense()->add_keys_vals(vals[i]);
+				}
+				m_DenseNodesGroup->mutable_dense()->add_keys_vals(0);
+
+				++nodeIt;
 			}
 
-			// we don't need the old string table anymore
-			delete m_StringTable;
-			m_StringTable = new StringTable();
+			m_DenseNodesGroup->clear_nodes();
 		}
 
-		// TODO prepare nodes
-
 		// prepare ways
-		{
+		if (m_WaysGroup) {
 			google::protobuf::RepeatedPtrField<Way>::iterator wayIt = m_WaysGroup->mutable_ways()->begin();
-			int realSize = 0;
+			int realSize;
 			while (wayIt != m_WaysGroup->mutable_ways()->end()) {
 				// encode and clean refs
 				realSize = deltaEncodeClean<int64_t>(wayIt->mutable_refs()->mutable_data(), wayIt->mutable_refs()->mutable_data() + wayIt->refs_size(), -1);
 				wayIt->mutable_refs()->Truncate(realSize);
 
-				// clean keys
-				realSize = cleanUp<uint32_t>(wayIt->mutable_keys()->mutable_data(), wayIt->mutable_keys()->mutable_data() + wayIt->keys_size(), 0);
-				wayIt->mutable_keys()->Truncate(realSize);
-
-				// clean vals
-				realSize = cleanUp<uint32_t>(wayIt->mutable_vals()->mutable_data(), wayIt->mutable_vals()->mutable_data() + wayIt->vals_size(), 0);
-				wayIt->mutable_vals()->Truncate(realSize);
-
-				// correct string ids
-				for (int i = 0; i < wayIt->keys_size(); i++) {
-					wayIt->set_keys(i, stringIdTable[wayIt->keys(i)]);
-					wayIt->set_vals(i, stringIdTable[wayIt->vals(i)]);
-				}
+				cleanUpTags<Way>(*wayIt, stringIdTable);
 
 				++wayIt;
 			}
 		}
 
+		delete[] stringIdTable;
+
 		output = m_PrimitiveBlock->SerializeAsString();
 
 		delete m_PrimitiveBlock;
 		m_PrimitiveBlock = new PrimitiveBlock();
+
+		m_PlainNodesGroup = NULL;
+		m_DenseNodesGroup = NULL;
+		m_WaysGroup = NULL;
+		m_RelationsGroup = NULL;
+
 		return true;
 	}
 
+	PrimitiveBlockOutputAdaptor & PrimitiveBlockOutputAdaptor::operator<<(INode & node) {
+		 createNode(node); return *this;
+	}
+
+	PrimitiveBlockOutputAdaptor & PrimitiveBlockOutputAdaptor::operator<<(IWay & way) {
+		 createWay(way); return *this;
+	}
 }
--- primitiveblockoutputadaptor.h
+++ primitiveblockoutputadaptor.h
@@ -3,12 +3,9 @@
 
 #include <cstdint>
 #include <string>
-#include <set>
+#include <deque>
 
-#include "onode.h"
-#include "oway.h"
-
-// #include "stringtable.h"
+#include "common.h"
 
 class PrimitiveBlock;
 class PrimitiveGroup;
@@ -16,6 +13,10 @@ class PrimitiveGroup;
 namespace osmpbf {
 	class INode;
 	class IWay;
+
+	class OWay;
+	class ONode;
+
 	class StringTable;
 
 	class PrimitiveBlockOutputAdaptor {
@@ -27,12 +28,12 @@ namespace osmpbf {
 		ONode createNode(INode & templateINode);
 		ONode createNode(INode & templateINode, NodeType type);
 
-		int nodesSize();
+		int nodesSize(NodeType type) const;
 
 		OWay createWay();
 		OWay createWay(const IWay & templateIWay);
 
-		int waysSize();
+		int waysSize() const;
 
 		void setGranularity(int32_t value);
 		void setLatOffset(int64_t value);
@@ -44,17 +45,20 @@ namespace osmpbf {
 
 		bool flush(std::string & output);
 
-// 		PrimitiveBlockOutputAdaptor & operator<<(INode & node) { createNode(node); return *this; }
-		inline PrimitiveBlockOutputAdaptor & operator<<(IWay & way) { createWay(way); return *this; }
+		PrimitiveBlockOutputAdaptor & operator<<(INode & node);
+		PrimitiveBlockOutputAdaptor & operator<<(IWay & way);
 
 	private:
 		PrimitiveBlock * m_PrimitiveBlock;
 		StringTable * m_StringTable;
 
-		PrimitiveGroup * m_NodesGroup;
+		PrimitiveGroup * m_PlainNodesGroup;
 		PrimitiveGroup * m_DenseNodesGroup;
 		PrimitiveGroup * m_WaysGroup;
 		PrimitiveGroup * m_RelationsGroup;
+
+		uint32_t * prepareStringTable();
+		void prepareNodes(PrimitiveGroup * nodesGroup, uint32_t * stringIdTable);
 	};
 }
 
