822b924b88c9d9494e949524b368d794f3798138
Oliver Groß <neokensuke@freenet.de>
2012-06-19 09:37:48 +0200

    added OSMFileIn class for reading osm pbf files (with an osm header)

---
+++ osmfile.cpp
@@ -0,0 +1,135 @@
+#include "osmfile.h"
+
+#include <iostream>
+
+#include "blobfile.h"
+#include "primitiveblockinputadaptor.h"
+
+#include "osmformat.pb.h"
+
+namespace osmpbf {
+
+// OSMFileIn
+
+	OSMFileIn::OSMFileIn(const std::string & fileName, bool verboseOutput) :
+		m_FileIn(new BlobFileIn(fileName)),
+		m_FileHeader(NULL),
+		m_DataOffset(0)
+	{
+		m_FileIn->setVerboseOutput(verboseOutput);
+	}
+
+	OSMFileIn::OSMFileIn(BlobFileIn * fileIn) :
+		m_FileIn(fileIn),
+		m_FileHeader(NULL),
+		m_DataOffset(0)
+	{}
+
+	OSMFileIn::~OSMFileIn() {
+		delete m_FileIn;
+		delete m_FileHeader;
+	}
+
+
+	bool OSMFileIn::open() {
+		if (m_FileIn->open() && parseHeader())
+			return true;
+
+		close();
+		return false;
+	}
+
+	void OSMFileIn::close() {
+		m_FileIn->close();
+		m_DataBuffer.clear();
+	}
+
+	void OSMFileIn::dataSeek(uint32_t position) {
+		m_FileIn->seek(m_DataOffset + position);
+	}
+
+	uint32_t OSMFileIn::dataPosition() const {
+		return m_FileIn->position() - m_DataOffset;
+	}
+
+	uint32_t OSMFileIn::dataSize() const {
+		return m_FileIn->size() - m_DataOffset;
+	}
+
+	uint32_t OSMFileIn::totalSize() const {
+		return m_FileIn->size();
+	}
+
+	bool OSMFileIn::parseBlock(PrimitiveBlockInputAdaptor & adaptor) {
+		m_FileIn->readBlob(m_DataBuffer);
+		adaptor.parseData(m_DataBuffer.data, m_DataBuffer.availableBytes);
+		return m_DataBuffer.type;
+	}
+
+	bool OSMFileIn::skipBlock() {
+		return m_FileIn->skipBlob();
+	}
+
+	bool OSMFileIn::parseHeader() {
+		m_FileIn->readBlob(m_DataBuffer);
+
+		if (m_DataBuffer.type != BLOB_OSMHeader) {
+			std::cerr << "ERROR: OSM header block not found" << std::endl;
+			return false;
+		}
+
+		m_FileHeader = new HeaderBlock;
+
+		if (!m_FileHeader->ParseFromArray(m_DataBuffer.data, m_DataBuffer.availableBytes)) {
+			std::cerr << "ERROR: invalid OSM header" << std::endl;
+			delete m_FileHeader;
+			m_FileHeader = NULL;
+			return false;
+		}
+
+		m_DataOffset = m_FileIn->position();
+		return true;
+	}
+
+	bool OSMFileIn::parserMeetsRequirements() const {
+		for (int i = 0; i < requiredFeaturesSize(); ++i) {
+			if ((m_FileHeader->required_features(i) != "OsmSchema-V0.6") ||
+				(m_FileHeader->required_features(i) != "DenseNodes"))
+				return false;
+		}
+
+		return true;
+	}
+
+	int OSMFileIn::requiredFeaturesSize() const {
+		return m_FileHeader->required_features_size();
+	}
+
+	int OSMFileIn::optionalFeaturesSize() const {
+		return m_FileHeader->optional_features_size();
+	}
+
+	const std::string & OSMFileIn::requiredFeatures(int index) const {
+		return m_FileHeader->required_features(index);
+	}
+
+	const std::string & OSMFileIn::optionalFeatures(int index) const {
+		return m_FileHeader->optional_features(index);
+	}
+
+	int64_t OSMFileIn::minLat() const {
+		return m_FileHeader->has_bbox() ? m_FileHeader->bbox().bottom() : 0;
+	}
+
+	int64_t OSMFileIn::maxLat() const {
+		return m_FileHeader->has_bbox() ? m_FileHeader->bbox().top() : 0;
+	}
+
+	int64_t OSMFileIn::minLon() const {
+		return m_FileHeader->has_bbox() ? m_FileHeader->bbox().left() : 0;
+	}
+
+	int64_t OSMFileIn::maxLon() const {
+		return m_FileHeader->has_bbox() ? m_FileHeader->bbox().right() : 0;
+	}
+}
---
+++ osmfile.h
@@ -0,0 +1,68 @@
+#ifndef OSMPBF_OSMFILE_H
+#define OSMPBF_OSMFILE_H
+
+#include <string>
+
+#include "blobdata.h"
+
+class HeaderBlock;
+
+namespace osmpbf {
+	class PrimitiveBlockInputAdaptor;
+	class BlobFileIn;
+
+	class OSMFileIn {
+	public:
+		OSMFileIn(const std::string & fileName, bool verboseOutput = false);
+		OSMFileIn(BlobFileIn * fileIn);
+		~OSMFileIn();
+
+
+		bool open();
+		void close();
+
+		void dataSeek(uint32_t position);
+		uint32_t dataPosition() const;
+		uint32_t dataSize() const;
+
+		uint32_t totalSize() const;
+
+		bool parseBlock(PrimitiveBlockInputAdaptor & adaptor);
+		bool skipBlock();
+
+		bool parserMeetsRequirements() const;
+
+		int requiredFeaturesSize() const;
+		int optionalFeaturesSize() const;
+
+		const std::string & requiredFeatures(int index) const;
+		const std::string & optionalFeatures(int index) const;
+
+		// bounding box in nanodegrees
+		int64_t minLat() const;
+		int64_t maxLat() const;
+
+		int64_t minLon() const;
+		int64_t maxLon() const;
+
+	protected:
+		BlobFileIn * m_FileIn;
+		BlobDataBuffer m_DataBuffer;
+
+		HeaderBlock * m_FileHeader;
+
+		uint32_t m_DataOffset;
+
+		bool parseHeader();
+
+	private:
+		OSMFileIn();
+		OSMFileIn(const OSMFileIn & other);
+		OSMFileIn & operator=(const OSMFileIn & other);
+	};
+
+	// TODO
+	class OSMFileOut;
+}
+
+#endif
