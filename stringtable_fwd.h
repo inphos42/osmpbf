9a6309c43ef7f96c011de946a3cbc836a11f1544
Oliver Groß <neokensuke@freenet.de>
2013-02-10 12:53:17 +0100

    updated generics submodule and applied api changes

--- stringtable_fwd.h
+++ stringtable_fwd.h
@@ -1,8 +1,7 @@
 #ifndef OSMPBF_STRINGTABLE_FDW_H
 #define OSMPBF_STRINGTABLE_FDW_H
-namespace generics {
-	template< typename > class Store;
-}
+
+#include <generics/store_fwd.h>
 
 namespace osmpbf {
 	typedef generics::Store< std::string > StringTable;
