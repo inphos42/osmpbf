20476e88d59bd086666a7f8f3d5ac913bf734906
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-03-14 14:00:24 +0100

    remove unused typedefs

--- typelimits.h
+++ typelimits.h
@@ -8,13 +8,9 @@ namespace osmpbf {
 #ifdef __LP64__
 typedef uint64_t OffsetType;
 typedef int64_t NegativeOffsetType;
-#define MAX_SIZE_FOR_FULL_MMAP 0xFFFFFFFF
-#define CHUNKED_MMAP_EXPONENT 23
 #else
 typedef uint32_t OffsetType;
 typedef int32_t NegativeOffsetType;
-#define MAX_SIZE_FOR_FULL_MMAP 0x3FFFFFFF
-#define CHUNKED_MMAP_EXPONENT 23
 #endif
 
 }//end namespace
