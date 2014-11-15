7363a911423103e76d47ecde9b3d4b3e39130b2d
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-11-15 02:23:27 +0100

    xmlconverter

---
+++ osmpbf/include/osmpbf/xmlconverter.h
@@ -0,0 +1,19 @@
+#ifndef OSMPBF_XML_CONVERTER_H
+#define OSMPBF_XML_CONVERTER_H
+#include <ostream>
+
+namespace osmpbf {
+
+class INode;
+class IWay;
+class IRelation;
+
+struct XmlConverter {
+	static std::ostream & print(std::ostream & out, const INode & node);
+	static std::ostream & print(std::ostream & out, const IWay & way);
+	static std::ostream & print(std::ostream & out, const IRelation & relation);
+};
+
+}//end namespace
+
+#endif
---
+++ osmpbf/xmlconverter.cpp
@@ -0,0 +1,54 @@
+#include <osmpbf/xmlconverter.h>
+#include <osmpbf/inode.h>
+#include <osmpbf/iway.h>
+#include <osmpbf/irelation.h>
+
+namespace osmpbf {
+
+void printTags(std::ostream & out, const IPrimitive & prim, const std::string & prefix) {
+	for(uint32_t i = 0, s = prim.tagsSize();  i < s; ++i) {
+		out << prefix << "<tag k=" << prim.key(i) << " v=" << prim.value(i) << ">\n";
+	}
+}
+
+std::string primitiveTypeToString(osmpbf::PrimitiveType t) {
+	switch (t) {
+	case osmpbf::PrimitiveType::NodePrimitive:
+		return "node";
+	case osmpbf::PrimitiveType::WayPrimitive:
+		return "way";
+	case osmpbf::PrimitiveType::RelationPrimitive:
+		return "relation";
+	default:
+		return "invalid";
+	};
+}
+
+std::ostream & XmlConverter::print(std::ostream & out, const INode & node) {
+	out << "<node id=" << node.id() << " lat=" << node.latd() << " lon=" << node.lond()  << ">\n";
+	printTags(out, node, "\t");
+	out << "</node>";
+	return out;
+}
+
+std::ostream & XmlConverter::print(std::ostream & out, const IWay & way) {
+	out << "<way id=" << way.id() << ">\n";
+	for(osmpbf::IWay::RefIterator refIt(way.refBegin()), refEnd(way.refEnd()); refIt != refEnd; ++refIt) {
+		out << "\t<nd ref=" << *refIt << "/>" << std::endl;
+	}
+	printTags(out, way, "\t");
+	out << "</way>";
+	return out;
+}
+
+std::ostream & XmlConverter::print(std::ostream & out, const IRelation & relation) {
+	out << "<relation id=" << relation.id() << ">\n";
+	for(osmpbf::IMemberStream mem(relation.getMemberStream()); !mem.isNull(); mem.next()) {
+		out << "\t<member type=" << primitiveTypeToString( mem.type() ) << " ref=" << mem.id() << " role=" << mem.role() << "/>\n";
+	}
+	printTags(out, relation, "\t");
+	out << "</relation>";
+	return out;
+}
+
+}//end namespace
